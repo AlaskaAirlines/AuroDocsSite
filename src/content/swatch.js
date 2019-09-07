540b73e5d8efdddacb5ec3700a11c2e4b255086e
Dale Sande <dale.sande@alaskaair.com>
2019-09-06 18:31:40 -0700

    fix: update colors to use data

--- src/content/swatch.js
+++ src/content/swatch.js
@@ -4,6 +4,10 @@ import "@alaskaairux/ods-swatch/dist/ods-swatch";
 import '../sass/App.scss';
 import data from "../../node_modules/@alaskaairux/orion-design-tokens/dist/tokens/TokenColorVariables.js";
 
+const brand = data.color.brand;
+const base = data.color.base;
+const background = data.color.background;
+
 class Swatch extends Component {
 
   // constructor(props) {
@@ -14,18 +18,17 @@ class Swatch extends Component {
   // };
 
   componentDidMount() {
-    const brand = data.color;
-    Object.keys(brand).forEach(function(key) {
-      // let data = brand[key];
-      // console.log(Object.keys(data));
-    });
+    //const brand = data.color.brand;
+    // Object.keys(brand).forEach(function(key) {
+    //   let data = brand[key];
+    //   console.log(Object.keys(data));
+    // });
 
-    const brandBlue = data.color.brand.blue;
-
-    Object.keys(brandBlue).forEach(function(key) {
-      //let data = brandBlue[key];
-      //console.log(Object.keys(data));
-    });
+    // const brandBlue = data.color.brand.blue;
+    // Object.keys(brandBlue).forEach(function(key) {
+    //   let data = brandBlue[key];
+    //   console.log(Object.keys(data));
+    // });
   }
 
 
@@ -56,61 +59,125 @@ class Swatch extends Component {
 
         <div className="util_swatchBox">
           <ods-swatch
-            backgroundcolor={data.color.brand.blue.midnight.base.original.value}
-            colorname={data.color.brand.blue.midnight.base.name}></ods-swatch>
+            backgroundcolor={brand.blue.midnight.base.value}
+            colorname={brand.blue.midnight.base.name}></ods-swatch>
         </div>
 
         <div className="util_swatchBox">
-          <ods-swatch backgroundcolor="0074c8" colorname="ColorBrandBlueAtlasBase"></ods-swatch>
-          <ods-swatch backgroundcolor="176cff" colorname="ColorBrandBlueAtlasLighten"></ods-swatch>
-          <ods-swatch backgroundcolor="0064c6" colorname="ColorBrandBlueAtlasDarken"></ods-swatch>
+          <ods-swatch
+            backgroundcolor={brand.blue.atlas.base.value}
+            colorname={brand.blue.atlas.base.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={brand.blue.atlas.lighten.value}
+            colorname={brand.blue.atlas.lighten.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={brand.blue.atlas.darken.value}
+            colorname={brand.blue.atlas.darken.name}></ods-swatch>
         </div>
 
         <div className="util_swatchBox">
-          <ods-swatch backgroundcolor="00C7e6" colorname="ColorBrandBlueBreezeBase"></ods-swatch>
-          <ods-swatch backgroundcolor="17d8ff" colorname="ColorBrandBlueBreezeLighten"></ods-swatch>
-          <ods-swatch backgroundcolor="00bae4" colorname="ColorBrandBlueBreezeDarken"></ods-swatch>
+          <ods-swatch
+            backgroundcolor={brand.blue.breeze.base.value}
+            colorname={brand.blue.breeze.base.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={brand.blue.breeze.lighten.value}
+            colorname={brand.blue.breeze.lighten.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={brand.blue.breeze.darken.value}
+            colorname={brand.blue.breeze.darken.name}></ods-swatch>
         </div>
 
         <div className="util_swatchBox">
-          <ods-swatch backgroundcolor="c0e585" colorname="ColorBrandGreenTropicalBase"></ods-swatch>
-          <ods-swatch backgroundcolor="bff380" colorname="ColorBrandGreenTropicalLighten"></ods-swatch>
-          <ods-swatch backgroundcolor="a6d36a" colorname="ColorBrandGreenTropicalDarken"></ods-swatch>
+          <ods-swatch
+            backgroundcolor={brand.green.tropical.base.value}
+            colorname={brand.green.tropical.base.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={brand.green.tropical.lighten.value}
+            colorname={brand.green.tropical.lighten.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={brand.green.tropical.darken.value}
+            colorname={brand.green.tropical.darken.name}></ods-swatch>
         </div>
 
         <div className="util_swatchBox">
-          <ods-swatch backgroundcolor="ffffff" colorname="ColorBaseWhite"></ods-swatch>
-          <ods-swatch backgroundcolor="222222" colorname="ColorBaseOrca"></ods-swatch>
-          <ods-swatch backgroundcolor="6e7783" colorname="ColorBaseShark"></ods-swatch>
-          <ods-swatch backgroundcolor="9fabbb" colorname="ColorBaseManatee"></ods-swatch>
-          <ods-swatch backgroundcolor="c8c9c7" colorname="ColorBaseFog"></ods-swatch>
-          <ods-swatch backgroundcolor="f7f7f7" colorname="ColorBaseCloud"></ods-swatch>
-          <ods-swatch backgroundcolor="e8e8e8" colorname="ColorBaseMist"></ods-swatch>
-          <ods-swatch backgroundcolor="336787" colorname="ColorBaseDusk"></ods-swatch>
-          <ods-swatch backgroundcolor="008568" colorname="ColorBaseGalapagos"></ods-swatch>
-          <ods-swatch backgroundcolor="df0b37" colorname="ColorBaseChili"></ods-swatch>
-          <ods-swatch backgroundcolor="def279" colorname="ColorBaseLimeaid"></ods-swatch>
-          <ods-swatch backgroundcolor="e5ecf0" colorname="ColorBaseBeluga"></ods-swatch>
-          <ods-swatch backgroundcolor="f26135" colorname="ColorBaseNemo"></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.white.value}
+            colorname={base.white.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.orca.value}
+            colorname={base.orca.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.shark.value}
+            colorname={base.shark.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.manatee.value}
+            colorname={base.manatee.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.fog.value}
+            colorname={base.fog.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.cloud.value}
+            colorname={base.cloud.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.mist.value}
+            colorname={base.mist.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.dusk.value}
+            colorname={base.dusk.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.galapagos.value}
+            colorname={base.galapagos.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.chili.value}
+            colorname={base.chili.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.limeade.value}
+            colorname={base.limeade.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.beluga.value}
+            colorname={base.beluga.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={base.nemo.value}
+            colorname={base.nemo.name}></ods-swatch>
         </div>
 
         <div className="util_swatchBox">
-          <ods-swatch backgroundcolor="f26135" colorname="ColorAccessibilityBorderActive"></ods-swatch>
-        </div>
-
-        <div className="util_swatchBox">
-          <ods-swatch backgroundcolor="01426a" colorname="ColorBackgroundBookingBar"></ods-swatch>
-          <ods-swatch backgroundcolor="01426a" colorname="ColorBackgroundFooter"></ods-swatch>
-          <ods-swatch backgroundcolor="01426a" colorname="ColorBackgroundAccountDrawer"></ods-swatch>
-          <ods-swatch backgroundcolor="01426a" colorname="ColorBackgroundLogo"></ods-swatch>
-          <ods-swatch backgroundcolor="ffffff" colorname="ColorBackgroundHeadline"></ods-swatch>
-          <ods-swatch backgroundcolor="ffffff" colorname="ColorBackgroundHeader"></ods-swatch>
-          <ods-swatch backgroundcolor="f7f7f7" colorname="ColorBackgroundSectionSeperator"></ods-swatch>
-          <ods-swatch backgroundcolor="df0b37" colorname="ColorBackgroundError"></ods-swatch>
-          <ods-swatch backgroundcolor="def279" colorname="ColorBackgroundAlert"></ods-swatch>
-          <ods-swatch backgroundcolor="e5ecf0" colorname="ColorBackgroundAdvisory"></ods-swatch>
-          <ods-swatch backgroundcolor="336787" colorname="ColorBackgroundDarkRule"></ods-swatch>
-          <ods-swatch backgroundcolor="c8c9c7" colorname="ColorBackgroundLightRule"></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.bookingBar.value}
+            colorname={background.bookingBar.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.footer.value}
+            colorname={background.footer.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.accountDrawer.value}
+            colorname={background.accountDrawer.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.logo.value}
+            colorname={background.logo.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.headline.value}
+            colorname={background.headline.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.header.value}
+            colorname={background.header.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.sectionSeperator.value}
+            colorname={background.sectionSeperator.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.error.value}
+            colorname={background.error.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.alert.value}
+            colorname={background.alert.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.advisory.value}
+            colorname={background.advisory.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.dark.rule.value}
+            colorname={background.dark.rule.name}></ods-swatch>
+          <ods-swatch
+            backgroundcolor={background.light.rule.value}
+            colorname={background.light.rule.name}></ods-swatch>
         </div>
 
       </section>
