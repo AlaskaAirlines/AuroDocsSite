bc3b0caeaad1e4dfbcaee8829e756b010a806d51
Dale Sande <dale.sande@alaskaair.com>
2019-09-20 13:50:14 -0700

    fix: update BACK TO TOP ui

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,39 +1,73 @@
-import React from 'react';
+import React, { Component } from "react";
 import { Link } from "react-scroll";
 import './style.scss';
 
-function Header() {
-  return (
-    <nav className="mainSiteNav" id="mainNav">
-      <Link className="hyperlink" activeClass="active" to="typography" spy={true} smooth={true} offset={-50} duration={2000}>
-        Typography
-      </Link>
+let lastScrollY = 0;
+let ticking = false;
 
-      <Link className="hyperlink" activeClass="active" to="buttons" spy={true} smooth={true} offset={-50} duration={2000}>
-        Buttons
-      </Link>
+class Nav extends Component {
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll, true);
+  };
 
-      <Link className="hyperlink" activeClass="active" to="icons" spy={true} smooth={true} offset={-50} duration={2000}>
-        Icons
-      </Link>
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  };
 
-      <Link className="hyperlink" activeClass="active" to="hyperlinks" spy={true} smooth={true} offset={-50} duration={2000}>
-        Hyperlinks
-      </Link>
+  option = React.createRef();
 
-      <Link className="hyperlink" activeClass="active" to="inputOptions" spy={true} smooth={true} offset={-50} duration={2000}>
-        Input Options
-      </Link>
+  handleScroll = () => {
+    lastScrollY = window.scrollY;
 
-      <Link className="hyperlink" activeClass="active" to="generator" spy={true} smooth={true} offset={-50} duration={2000}>
-        WC Generator
-      </Link>
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        console.log(lastScrollY);
 
-      <Link className="floatingTopLink" activeClass="active" to="mainNav" spy={true} smooth={true} offset={-50} duration={2000}>
-        Back To Top
-      </Link>
-    </nav>
-  );
+        if (lastScrollY < 500) {
+          this.option.current.style.display = `none`;
+        } else if (lastScrollY > 500){
+          this.option.current.style.display = `block`;
+        }
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  };
+
+  render() {
+    return (
+      <nav className="mainSiteNav" id="mainNav">
+        <Link className="hyperlink" activeClass="active" to="typography" spy={true} smooth={true} offset={-50} duration={2000}>
+          Typography
+        </Link>
+
+        <Link className="hyperlink" activeClass="active" to="buttons" spy={true} smooth={true} offset={-50} duration={2000}>
+          Buttons
+        </Link>
+
+        <Link className="hyperlink" activeClass="active" to="icons" spy={true} smooth={true} offset={-50} duration={2000}>
+          Icons
+        </Link>
+
+        <Link className="hyperlink" activeClass="active" to="hyperlinks" spy={true} smooth={true} offset={-50} duration={2000}>
+          Hyperlinks
+        </Link>
+
+        <Link className="hyperlink" activeClass="active" to="inputOptions" spy={true} smooth={true} offset={-50} duration={2000}>
+          Input Options
+        </Link>
+
+        <Link className="hyperlink" activeClass="active" to="generator" spy={true} smooth={true} offset={-50} duration={2000}>
+          WC Generator
+        </Link>
+
+        <Link activeClass="active" to="mainNav" spy={true} smooth={true} offset={-50} duration={2000}>
+          <span className="floatingTopLink util_displayHidden" ref={this.option}>Back To Top</span>
+        </Link>
+      </nav>
+    );
+  };
 }
 
-export default Header;
+export default Nav;
