a7564be987b0185d024ba5f9545e670d54b6aeb8
Dale Sande <dale.sande@alaskaair.com>
2020-06-22 12:16:29 -0700

    fix: remove in-discovery work from status view in favor of audit view

--- src/content/dynamic/componentStatus/statusView.js
+++ src/content/dynamic/componentStatus/statusView.js
@@ -3,42 +3,6 @@ import { Query } from '@apollo/react-components';
 import { gql } from 'apollo-boost';
 import Issue from './issue';
 
-const componentStatus = gql`
-{
-  organization(login: "AlaskaAirlines") {
-    team(slug: "auro-team") {
-      repositories(first: 20, orderBy: {field: NAME, direction: ASC}) {
-        nodes {
-          name
-          issues(last: 20, orderBy: {field: CREATED_AT, direction: ASC}, states: OPEN, filterBy: {labels: "Status: Discovery"}) {
-            nodes {
-              title
-              state
-              url
-              number
-              labels(first: 10, orderBy: {field: NAME, direction: ASC}) {
-                nodes {
-                  name
-                  color
-                  description
-                }
-              }
-              projectCards {
-                nodes {
-                  column {
-                    name
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`
-
 const workInProgress = gql`
 {
   organization(login: "AlaskaAirlines") {
@@ -134,21 +98,6 @@ class PlannedWork extends Component {
         <h2 className="auro_heading auro_heading--700">Approved for Work</h2>
         <p>The following lists are of tools and components currently approved for the Auro roadmap.</p>
         <Query query={approvedForWork}>
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>We are unable to connect to GitHub at the moment, please try back later.</p>;
-
-            return data.organization.team.repositories.nodes.map(({ name, issues }) => (
-              issues.nodes.length > 0
-                ? <Issue key={name} name={name} issues={issues.nodes} />
-                : ''
-            ));
-          }}
-        </Query>
-
-        <h2 className="auro_heading auro_heading--700">Proposed work in discovery</h2>
-        <p>The following is a list of all proposed web components and other request(s) currently proposed for discovery.</p>
-        <Query query={componentStatus}>
           {({ loading, error, data }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>We are unable to connect to GitHub at the moment, please try back later.</p>;
