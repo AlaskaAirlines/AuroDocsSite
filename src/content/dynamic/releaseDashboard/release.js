50fdc68bec831f008fde70f8f3f62db7ea881af4
Dale Sande <dale.sande@alaskaair.com>
2020-06-23 17:30:03 -0700

    fix: remove unsed reference

--- src/content/dynamic/releaseDashboard/release.js
+++ src/content/dynamic/releaseDashboard/release.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactMarkdown from 'react-markdown';
 import CodeBlock from 'components/CodeBlock';
 import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
