0ed99b138913c7822d78825cc9e3b6ca186a96ac
Jason Baker <jason@capsule42.com>
2021-04-16 09:11:48 -0700

    refactor: add updates suggested from feedback
    Co-authored-by: Dale Sande / @anotheruiguy <dale.sande@gmail.com>

--- src/content/dynamic/wcss/nav.js
+++ src/content/dynamic/wcss/nav.js
@@ -7,12 +7,12 @@ export class Nav extends Component {
     return (
       <div role="tablist" className="tabList">
         <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets`} activeClassName="is-active">WCSS</NavLink>
-        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/features`} activeClassName="is-active">WCSS Features</NavLink>
-        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/custom-element-css`} activeClassName="is-active">CSS & Custom Elements</NavLink>
-        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/conventions`} activeClassName="is-active">CSS Conventions</NavLink>
-        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/guidelines`} activeClassName="is-active">Style Guidelines</NavLink>
-        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/tests`} activeClassName="is-active">Sass Unit Tests</NavLink>
-        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/linter`} activeClassName="is-active">Stylelint Rules</NavLink>
+        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/features`} activeClassName="is-active">WCSS features</NavLink>
+        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/custom-element-css`} activeClassName="is-active">CSS & custom elements</NavLink>
+        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/conventions`} activeClassName="is-active">CSS conventions</NavLink>
+        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/guidelines`} activeClassName="is-active">Style guidelines</NavLink>
+        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/tests`} activeClassName="is-active">Sass unit tests</NavLink>
+        <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/linter`} activeClassName="is-active">Stylelint rules</NavLink>
 
         {/* <NavLink role="tab" exact className="tab link" to={`/webcorestylesheets/faq`} activeClassName="is-active">F.A.Q.</NavLink> */}
       </div>
