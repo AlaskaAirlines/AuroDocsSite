38190fa2be7cb48c35ef3de6e14a3032cc58390c
Dale Sande <dale.sande@gmail.com>
2021-06-14 13:59:42 -0700

    style: change tab nav color; remove unsed import ref

--- src/content/dynamic/generator/commonDefs.js
+++ src/content/dynamic/generator/commonDefs.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Nav } from './nav';
 import ReactMarkdown from 'react-markdown';
 import CodeBlock from 'components/CodeBlock';
 import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
