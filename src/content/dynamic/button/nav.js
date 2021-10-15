57e4c492ec401c2ae4ff98735c55eeec50efaa3d
Dale Sande <dale.sande@gmail.com>
2021-10-15 10:31:06 -0700

    docs: add auro-button figma link and release notes

--- src/content/dynamic/button/nav.js
+++ src/content/dynamic/button/nav.js
@@ -10,6 +10,7 @@ export class Nav extends Component {
         <NavLink role="tab" exact className="tab link" to={`/components/auro/button/install`} activeClassName="is-active">Install</NavLink>
         <NavLink role="tab" exact className="tab link" to={`/components/auro/button/api`} activeClassName="is-active">API</NavLink>
         <NavLink role="tab" exact className="tab link" to={`/components/auro/button/releases`} activeClassName="is-active">Releases</NavLink>
+        <NavLink role="tab" exact className="tab link" to={`/components/auro/button/figma`} activeClassName="is-active">Design support</NavLink>
       </div>
     )
   }
