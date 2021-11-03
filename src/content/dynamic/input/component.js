6ac0430c1d36a5c5217e6c4515628472c704feb3
Dale Sande <dale.sande@gmail.com>
2021-11-03 12:53:30 -0700

    docs: update component page to point to demo.md from repo

--- src/content/dynamic/input/component.js
+++ src/content/dynamic/input/component.js
@@ -1,253 +1,61 @@
-import React, { Component } from "react";
+import React from "react";
 import { Nav } from './nav';
-import Highlight from 'react-highlight';
-import 'highlight.js/styles/github.css';
+import marked from 'marked';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
 
-class AuroInput extends Component {
+const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-input/main/demo/demo.md';
 
-  // constructor(props) {
-  //   super(props);
-  // };
+class AuroButton extends MarkdownPageWrapper {
+
+  // function to get text from MD document
+  getMarkdownText() {
+    fetch(markdownContent)
+        .then((response) => response.text())
+        .then((text) => {
+          const rawHtml = marked(text);
+          document.querySelector('.auro-markdown').innerHTML = rawHtml;
+          Prism.highlightAll();
+        });
+
+    const renderer = new marked.Renderer();
+    renderer.link = function(href, title, text) {
+      const link = marked.Renderer.prototype.link.call(this, href, title, text);
+      let url = href
+      url = url.replace(/^.*\/\/[^/]+/, '')
+
+      if (href.includes("auro.alaskaair.com")) {
+
+        return link.replace("href",`href="${url}"`);
+      } else {
+
+        const newLink = `<a href="${href}"  rel="noopener noreferrer" target="_blank" className="externalLink">${text} <auro-icon customColor category="interface" name="external-link-md"></auro-icon></a>`
+
+        return newLink;
+      }
+    };
+
+    marked.setOptions({
+        renderer: renderer
+    });
+  }
+
+
 
   render() {
     return (
-      <section id="component">
+      <section className="auro_baseType">
 
         <Nav />
 
-        <auro-header level="2" display="display">Input</auro-header>
-
-        <p>Use the <code>auro-input</code> custom element to create basic single-line text fields. Supports type <code>text</code>, <code>password</code>, and <code>email</code> with validation, required input and error states.</p>
-
-        <auro-header level="2" display="700">Default component</auro-header>
-
-        <p>The default component supports the basic input <code>type="text"</code> structure. The <code>(optional)</code> label is provided to instruct the user that their input is not required.</p>
-
-        <div className="demo--inline exampleWrapper">
-          <auro-input id="demo"></auro-input>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`/* For accessibility, always be sure to address the ID attribute  */
-<auro-input id="demo"></auro-input>`}
-          </Highlight>
-        </auro-accordion>
-
-        <p>Use the <code>label</code> attribute to add content to the input label. Use the <code>helptext</code> attribute to add content under the input to assist the user with the type of input required. </p>
-
-        <div className="demo--inline exampleWrapper">
-          <auro-input label="Name" id="full-name" helptext="Please enter your full name"></auro-input>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-input label="Name" id="full-name" helptext="Please enter your full name"></auro-input>`}
-          </Highlight>
-        </auro-accordion>
-
-        <p>Use the <code>required</code> attribute to enable input validation.</p>
-
-        <div className="demo--inline exampleWrapper">
-          <auro-input required label="Name" id="full-name" helptext="Please enter your full name"></auro-input>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-input required label="Name" id="full-name" helptext="Please enter your full name"></auro-input>`}
-          </Highlight>
-        </auro-accordion>
-
-        <p>The default error return of <code>Please fill out this field</code> will be used if a custom <code>error</code> message is not added to the element on blur.</p>
-
-        <div className="demo--inline exampleWrapper">
-          <auro-input required label="Name" id="full-name" helptext="Please enter your full name" error="Please provide your full name."></auro-input>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-input
-  required
-  label="Name"
-  id="full-name"
-  helptext="Please enter your full name"
-  error="Please provide your full name">
-</auro-input>`}
-          </Highlight>
-        </auro-accordion>
-
-        <auro-header level="2" display="700">Password</auro-header>
-
-        <p>Use the <code>type="password"</code> attribute for a password style input.</p>
-
-        <div className="demo--inline exampleWrapper">
-          <auro-input required type="password" id="password" label="Password" helptext="Please enter a secure password."></auro-input>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-input
-  required
-  type="password"
-  id="password"
-  label="Password"
-  helptext="Please enter a secure password.">
-</auro-input>`}
-          </Highlight>
-        </auro-accordion>
-
-        <auro-header level="2" display="700">Email</auro-header>
-
-        <p>Use the <code>type="email"</code> attribute for a email style input.</p>
-
-        <div className="demo--inline exampleWrapper">
-          <auro-input required type="email" label="Email address" id="email-address" helptext="Please enter your email address"></auro-input>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-input
-  required
-  type="email"
-  label="Email address"
-  id="email-address"
-  helptext="Please enter your email address">
-</auro-input>`}
-          </Highlight>
-        </auro-accordion>
-
-        <auro-header level="2" display="700">Credit Card</auro-header>
-
-        <p>Use the <code>type="credit-card"</code> attribute for a credit card formatted input.</p>
-
-        <div className="demo--inline exampleWrapper">
-          <auro-input type="credit-card" label="Card number" id="credit-card" helptext="Please enter your credit card number" required></auro-input>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-input
-  type="credit-card"
-  label="Card number"
-  id="credit-card"
-  helptext="Please enter your credit card number"
-  required>
-</auro-input>`}
-          </Highlight>
-        </auro-accordion>
-
-        <p>Use the <code>type="credit-card"</code> and <code>icon</code> attributes for a credit card formatted input with credit card icon support.</p>
-
-        <div className="demo--inline exampleWrapper">
-          <auro-input type="credit-card" label="Card number" id="credit-card" helptext="Please enter your credit card number" required icon></auro-input>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`// Use 4147 3411 1111 1111 to see the Alaska Airline's credit card!
-<auro-input
-  type="credit-card"
-  label="Card number"
-  id="credit-card"
-  helptext="Please enter your credit card number"
-  required
-  icon>
-</auro-input>`}
-          </Highlight>
-        </auro-accordion>
-
-        <auro-header level="2" display="700">Disabled</auro-header>
-
-        <p>All use cases for auro-input support the <code>disabled</code> attribute.</p>
-
-        <div className="demo--inline exampleWrapper">
-          <auro-input disabled required label="Address" id="address" helptext="Please enter your home address"></auro-input>
-          <auro-input disabled required type="email" id="email-address" label="Email address" helptext="Please enter your email address"></auro-input>
-          <auro-input disabled required type="password" id="password" label="Password" helptext="Please enter a secure password"></auro-input>
-          <auro-input disabled required type="credit-card" id="credit-card" label="Card number" helptext="Please enter your credit card number"></auro-input>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-input
-  disabled
-  required
-  label="Address"
-  id="address"
-  helptext="Please enter your home address">
-</auro-input>
-
-<auro-input
-  disabled
-  required
-  type="email"
-  id="email-address"
-  label="Email address"
-  helptext="Please enter your email address">
-</auro-input>
-
-<auro-input
-  disabled
-  required
-  type="password"
-  id="password"
-  label="Password"
-  helptext="Please enter a secure password">
-</auro-input>
-
-<auro-input
-  disabled
-  required
-  type="credit-card"
-  id="credit-card"
-  label="Card number"
-  helptext="Please enter your credit card number">
-</auro-input>`}
-          </Highlight>
-        </auro-accordion>
-
-        <auro-header level="2" display="700">Disable auto validation</auro-header>
-
-        <p>For use cases where the field is <code>required</code>, but live validation is not wanted, use the <code>noValidate</code> attribute.</p>
-
-        <div className="demo--inline exampleWrapper">
-          <auro-input noValidate required label="Address" id="address" helptext="Please enter your home address"></auro-input>
-          <auro-input noValidate required type="email" id="email-address" label="Email address" helptext="Please enter your email address"></auro-input>
-          <auro-input noValidate required type="credit-card" id="credit-card" label="Card number" helptext="Please enter your credit card number"></auro-input>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-input
-  noValidate
-  required
-  label="Address"
-  id="address"
-  helptext="Please enter your home address">
-</auro-input>
-
-<auro-input
-  noValidate
-  required
-  type="email"
-  id="email-address"
-  label="Email address"
-  helptext="Please enter your email address">
-</auro-input>
-
-<auro-input
-  noValidate
-  required
-  type="credit-card"
-  id="credit-card"
-  label="Card number"
-  helptext="Please enter your credit card number">
-</auro-input>`}
-          </Highlight>
-        </auro-accordion>
+        <section
+          className="auro-markdown"
+          dangerouslySetInnerHTML={this.getMarkdownText()}
+        />
       </section>
     );
   }
 }
 
-export default AuroInput;
+export default AuroButton;
