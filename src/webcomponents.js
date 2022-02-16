5523af3c644af7317c20bcf3bd2ae5dbe3e2df9c
Dale Sande <dale.sande@gmail.com>
2022-02-16 12:10:02 -0800

    docs: update to use latest version of badge
    Changes to be committed:
    modified:   package-lock.json
    modified:   package.json
    modified:   public/index.html
    modified:   src/webcomponents.js

--- src/webcomponents.js
+++ src/webcomponents.js
@@ -1,9 +1,6 @@
 
 // All Auro web components need to be listed here to be packaged properly for IE11 support
 
-// badge
-import "@alaskaairux/auro-badge";
-
 // loader
 import "@alaskaairux/auro-loader";
 
