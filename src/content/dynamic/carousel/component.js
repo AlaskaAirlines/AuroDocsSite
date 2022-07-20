8d44d2f402e5897dcb03aabb7a991c76ae9c1e38
Dale Sande <dale.sande@gmail.com>
2022-07-20 11:29:21 -0700

    docs(carousel): update demo page
    Changes to be committed:
    modified:   src/content/dynamic/carousel/component.js

--- src/content/dynamic/carousel/component.js
+++ src/content/dynamic/carousel/component.js
@@ -10,8 +10,9 @@ class AuroCarousel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedDay: 3,
-      anotherSelectedDay: 4
+      selectedDay: 0,
+      anotherSelectedDay: 0,
+      centerSelectedDay: 0
     };
     this.centerCarouselRef = React.createRef();
   }
@@ -52,6 +53,7 @@ class AuroCarousel extends Component {
         </Highlight>
 
         <auro-header level="2" display="700">Default carousel with auro-pane element</auro-header>
+        <p>The following example illustrates a the default behavior of the carousel element. Notice how the toggle buttons on the left and the right only appear when there are enough nodes in that direction. This element is fully responsive whereas the l/r toggles will not appear on a mobile device and finger-swipe will operate as expected. </p>
         <div className="demo--inline exampleWrapper">
           <auro-carousel label="Flight options">
             {days.map((day, idx) => (
@@ -82,6 +84,7 @@ class AuroCarousel extends Component {
         </auro-accordion>
 
         <auro-header level="2" display="700">Carousel with static toggles</auro-header>
+        <p>The following example uses the <code>displayArrows</code> attribute to allow the l/r toggle buttons to ALWAYS appear. NOTE: this attribute has no impact on the mobile experience.</p>
         <div className="demo--inline exampleWrapper">
           <auro-carousel label="Flight options" displayArrows>
             {days.map((day, idx) => (
@@ -112,6 +115,8 @@ class AuroCarousel extends Component {
         </auro-accordion>
 
         <auro-header level="2" display="700">Carousel that calls centerElement onClick</auro-header>
+        <p>The following example illustrates the use of the <code>centerElement</code> method. This feature will attempt top center the selected pane element when clicked if there are enough surrounding nodes. This example DOES NOT center on load.</p>
+        <p>This method requires the user to invoke this feature based on a click event. See both a vanilla JS and React example below.</p>
         <div className="demo--inline exampleWrapper">
           <auro-carousel label="Flight options" ref={this.centerCarouselRef}>
             {days.map((day, idx) => (
@@ -130,7 +135,7 @@ class AuroCarousel extends Component {
         <auro-accordion lowProfile justifyRight>
           <span slot="trigger">See code</span>
           <Highlight className='html afterCode'>
-            {`<auro-carousel label="Flight options">
+            {`<auro-carousel label="Flight options" id="centerElement">
   <auro-pane date="2020-01-01"></auro-pane>
   <auro-pane date="2020-01-02"></auro-pane>
   <auro-pane date="2020-01-03"></auro-pane>
@@ -139,8 +144,41 @@ class AuroCarousel extends Component {
   <auro-pane date="2020-01-06"></auro-pane>
 
   ...
+</auro-carousel>
 
-</auro-carousel>`}
+// Vanilla Javascript example
+// -------------------------
+<script type="module">
+  const carousel = document.querySelector('auro-carousel#centerElement');
+  const panes = carousel.querySelectorAll('auro-pane');
+  let selectedPane = carousel.querySelector('auro-pane[selected]');
+
+  panes.forEach(pane => pane.addEventListener('click', () => {
+    selectedPane.selected = false;
+    pane.selected = true;
+    selectedPane = pane;
+    carousel.centerElement(selectedPane);
+  }));
+</script>
+
+// React example
+// -------------------------
+const days = [...Array(28).keys()];
+
+<auro-carousel label="Flight options" ref={this.centerCarouselRef}>
+  {days.map((day, idx) => (
+    <auro-pane
+      key={day}
+      date={\`2021-10-\${(day + 1).toString().padStart(2, 0)}\`}
+      selected={this.state.centerSelectedDay === idx ? true : undefined}
+      onClick={({ target }) => {
+        this.setState({centerSelectedDay: idx});
+        this.centerCarouselRef.current.centerElement(target);
+      }}>
+    </auro-pane>
+  ))}
+</auro-carousel>
+`}
           </Highlight>
         </auro-accordion>
 
