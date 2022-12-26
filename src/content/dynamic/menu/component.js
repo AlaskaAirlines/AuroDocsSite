1e717d5b8727b10b360de20a2fc8dba2af65c8e7
Jason Baker <jason@capsule42.com>
2022-12-26 09:13:12 -0800

    refactor(auro-menu): use correct namespace #102

--- src/content/dynamic/menu/component.js
+++ src/content/dynamic/menu/component.js
@@ -3,7 +3,6 @@ import { Nav } from './nav';
 import marked from 'marked';
 import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
-// import markdownContent from '@alaskaairux/auro-menu/demo/demo.md'
 import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
 
 const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-menu/master/demo/demo.md';
