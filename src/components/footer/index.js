fa7cfcf7365bdcd4deafb1cdda22489c43e89396
Dale Sande <dale.sande@gmail.com>
2023-11-15 11:41:51 -0800

    fix: remove unnecessary aria roles from header/footer
    Changes to be committed:
    modified:   src/components/footer/index.js
    modified:   src/components/header/index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer className="auro_util_body--sm" role="contentinfo">
+      <footer className="auro_util_body--sm">
         <p>© Copyright 2019-present Alaska Airlines, inc. All rights reserved.</p>
       </footer>
     );
