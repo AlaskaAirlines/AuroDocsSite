ea2c5fca4a65b5e05df5727d0c44f4b648fd300c
Dale Sande <dale.sande@gmail.com>
2024-01-24 17:32:36 -0800

    docs: update lockup to use apiExamples doc
    Changes to be committed:
    modified:   src/content/dynamic/lockup/api.js

--- src/content/dynamic/lockup/api.js
+++ src/content/dynamic/lockup/api.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Nav } from './nav';
 import ReactMarkdown from 'react-markdown';
 import CodeBlock from 'components/CodeBlock';
-// import markdownContent from '@aurodesignsystem/auro-lockup/docs/api.md'
+// import markdownContent from '@aurodesignsystem/auro-lockup/demo/apiExamples.md'
 import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
 
-const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-lockup/master/docs/api.md';
+const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-lockup/main/demo/apiExamples.md';
 
 class AuroAlertsApi extends MarkdownPageWrapper {
 
