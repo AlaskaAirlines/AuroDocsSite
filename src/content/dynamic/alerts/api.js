42b55d9b26479373647dde08d80c4ebc80903056
fajar-apri-alaska <Fajar.Apriansyah@alaskaair.com>
2024-02-02 15:29:14 +0700

    docs(alert): update API markdown path
    Changes to be committed:
    	modified:   src/content/dynamic/alerts/api.js

--- src/content/dynamic/alerts/api.js
+++ src/content/dynamic/alerts/api.js
@@ -4,7 +4,7 @@ import ReactMarkdown from 'react-markdown';
 import CodeBlock from 'components/CodeBlock';
 import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
 
-const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-alerts/master/docs/api.md';
+const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-alerts/master/demo/api.md';
 
 class AuroAlertsApi extends MarkdownPageWrapper {
 
