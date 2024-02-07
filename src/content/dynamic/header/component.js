96dfb38e500daad2bb0018fa41d4061394a01904
Jason Baker <jason@capsule42.com>
2024-02-07 07:40:55 -0600

    refactor(releases): cleanup release page code per review comments

--- src/content/dynamic/header/component.js
+++ src/content/dynamic/header/component.js
@@ -5,7 +5,7 @@ class AuroContent extends AuroComponentContent {
   constructor(props) {
     super(props);
 
-    this.markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-header/main/demo/demo.md';
+    this.markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-header/master/demo/demo.md';
   };
 }
 
