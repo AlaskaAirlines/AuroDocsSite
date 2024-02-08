bb656027683442a5b6f18d2fee410bfcfb9d263c
Jason Baker <jason@capsule42.com>
2024-02-08 13:23:08 -0600

    refactor(select): make select pages now use new MD renderer

--- src/content/dynamic/select/install.js
+++ src/content/dynamic/select/install.js
@@ -1,43 +1,13 @@
-import React from "react";
-import { Nav } from './nav';
-import ReactMarkdown from 'react-markdown';
-import CodeBlock from 'components/CodeBlock';
-// import markdownContent from '@aurodesignsystem/auro-select/README.md'
-import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
+import AuroComponentContent from "functions/renderContentPage";
 
-const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-select/master/README.md';
+class AuroContent extends AuroComponentContent {
 
-class AuroSelectInstall extends MarkdownPageWrapper {
+  constructor(props) {
+    super(props);
 
-  // function to get text from MD document
-  componentWillMount() {
-    fetch(markdownContent).then((response) => response.text()).then((text) => {
-      this.setState({
-        contentBuild: text
-      })
-    })
-  }
-
-  render() {
-    return (
-      <section className="auro_baseType">
-
-        <Nav />
-
-        <section className="auro-markdown">
-          <ReactMarkdown
-            source={this.state.contentBuild}
-            escapeHtml={false}
-            renderers={{
-              code: CodeBlock,
-              heading: this.headingRenderer,
-              link: this.linkRenderer
-            }}/>
-        </section>
-
-      </section>
-    );
-  }
+    this.hasFigma = true;
+    this.markdownContentPath = 'README.md';
+  };
 }
 
-export default AuroSelectInstall;
+export default AuroContent;
