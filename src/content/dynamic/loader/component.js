8a780f01911c280825609d1d0c629d85a61593c5
Dale Sande <dale.sande@gmail.com>
2024-02-10 23:14:12 -0800

    docs: update loader demo ref
    Changes to be committed:
    modified:   src/content/dynamic/loader/component.js

--- src/content/dynamic/loader/component.js
+++ src/content/dynamic/loader/component.js
@@ -1,136 +1,58 @@
-import React, { Component } from "react";
+import React from "react";
 import { Nav } from './nav';
-import Highlight from 'react-highlight';
-import 'highlight.js/styles/github.css';
+import marked from 'marked';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
 
-class AuroLoader extends Component {
+const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-loader/master/demo/demo.md';
 
-  // constructor(props) {
-  //   super(props);
-  // };
+class AuroLoader extends MarkdownPageWrapper {
+
+  // function to get text from MD document
+  getMarkdownText() {
+    fetch(markdownContent)
+        .then((response) => response.text())
+        .then((text) => {
+          const rawHtml = marked(text);
+          document.querySelector('.auro-markdown').innerHTML = rawHtml;
+          Prism.highlightAll();
+        });
+
+    const renderer = new marked.Renderer();
+    renderer.link = function(href, title, text) {
+      const link = marked.Renderer.prototype.link.call(this, href, title, text);
+      let url = href
+      url = url.replace(/^.*\/\/[^/]+/, '')
+
+      if (href.includes("auro.alaskaair.com")) {
+
+        return link.replace("href",`href="${url}"`);
+      } else {
+
+        const newLink = `<a href="${href}"  rel="noopener noreferrer" target="_blank" className="externalLink">${text} <auro-icon customColor category="interface" name="external-link-md"></auro-icon></a>`
+
+        return newLink;
+      }
+    };
+
+    marked.setOptions({
+        renderer: renderer
+    });
+  }
+
+
 
   render() {
     return (
-      <section id="component">
+      <section className="auro_baseType">
 
         <Nav />
 
-        <auro-header level="2" display="display">auro-loader</auro-header>
-
-        <p>Things take time. Sometimes we just need to take a second for some data to show up. For this Auro supports the <code>auro-loader</code> custom element. This powerful element supports multiple modes for use. </p>
-
-        <p>
-          <code>Ringworm</code>, <code>Orbit</code>, <code>Pulse</code>, and <code>Laser</code>.
-        </p>
-
-        <p>And each of these modes support five size settings.</p>
-
-        <p><code>default</code>, <code>sm</code>, <code>md</code>, <code>lg</code>, and <code>xl</code></p>
-
-        <p>For color support <code>auro-loader</code> supports <code>currentColor</code>. This allows for any color set on the parent element to influence the color of the loader.</p>
-
-        <p>The <code>auro-loader</code> custom element also supports three pre-defined color modes.</p>
-
-        <p>
-          <code>onlight</code>, <code>ondark</code> and <code>white</code>.
-        </p>
-
-        <auro-header level="2" display="700">Loader gallery</auro-header>
-        <p>See below all the modes of loaders this element supports. </p>
-        <div className="demo--inline exampleWrapper">
-          <auro-loader ringworm md></auro-loader>
-          <auro-loader orbit md></auro-loader>
-          <auro-loader pulse md></auro-loader>
-          <auro-loader laser></auro-loader>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-loader ringworm md></auro-loader>
-<auro-loader orbit md></auro-loader>
-<auro-loader pulse md></auro-loader>
-<auro-loader laser></auro-loader>`}
-          </Highlight>
-        </auro-accordion>
-
-        <auro-header level="2" display="700">Loader sizes</auro-header>
-        <p>The auro-loader element supports a scale of sizes.</p>
-        <div className="demo--inline exampleWrapper">
-          <auro-loader ringworm></auro-loader>
-          <auro-loader ringworm sm></auro-loader>
-          <auro-loader ringworm md></auro-loader>
-          <auro-loader ringworm lg></auro-loader>
-          <auro-loader ringworm xl></auro-loader>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-loader ringworm></auro-loader>
-<auro-loader ringworm sm></auro-loader>
-<auro-loader ringworm md></auro-loader>
-<auro-loader ringworm lg></auro-loader>
-<auro-loader ringworm xl></auro-loader>`}
-          </Highlight>
-        </auro-accordion>
-
-        <auro-header level="2" display="700">Color support</auro-header>
-        <div className="demo--inline exampleWrapper">
-          <auro-loader pulse onlight md></auro-loader>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-loader orbit onlight md></auro-loader>`}
-          </Highlight>
-        </auro-accordion>
-
-        <div className="demo--inline exampleWrapper--ondark">
-          <auro-loader pulse ondark sm></auro-loader><br/>
-          <auro-loader pulse white sm></auro-loader>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-loader pulse ondark sm></auro-loader>
-<auro-loader pulse white sm></auro-loader>`}
-          </Highlight>
-        </auro-accordion>
-
-        <auro-header level="2" display="700">Custom color support</auro-header>
-        <div className="demo--inline exampleWrapper">
-          <auro-loader style={{color: 'pink'}} ringworm md></auro-loader>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='html afterCode'>
-            {`<auro-loader style={{color: 'pink'}} ringworm md></auro-loader>`}
-          </Highlight>
-        </auro-accordion>
-
-        <auro-header level="2" display="700">Custom animation speed</auro-header>
-        <p>
-          The <code>auro-loader</code> custom element has a CSS shadow part included in it's API. Create a custom selector using <code>::part(element)</code> and easily adjust the <code>animation-duration</code> as seen in the examples below.
-        </p>
-        <div className="demo--inline exampleWrapper--ondark">
-          <auro-loader class="slow" ondark orbit lg></auro-loader>
-          <auro-loader class="fast" white orbit xl></auro-loader>
-        </div>
-        <auro-accordion lowProfile justifyRight>
-          <span slot="trigger">See code</span>
-          <Highlight className='css afterCode'>
-            {`.slow::part(element) {
-  animation-duration: 8s;
-}
-
-.fast::part(element) {
-  animation-duration: 1s;
-}`}
-          </Highlight>
-          <Highlight className='html afterCode'>
-            {`<auro-loader class="slow" ondark orbit lg></auro-loader>
-<auro-loader class="fast" white orbit xl></auro-loader>`}
-          </Highlight>
-        </auro-accordion>
+        <section
+          className="auro-markdown"
+          dangerouslySetInnerHTML={this.getMarkdownText()}
+        />
       </section>
     );
   }
