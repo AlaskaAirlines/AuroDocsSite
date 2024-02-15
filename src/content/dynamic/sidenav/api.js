15fa54fee540530defdb4919beea546d7475ac87
Jordan Jones <jmjones2463@gmail.com>
2024-02-15 13:07:25 -0800

    perf(sidenav): update sidenav demo file names

--- src/content/dynamic/sidenav/api.js
+++ src/content/dynamic/sidenav/api.js
@@ -5,7 +5,7 @@ import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
 import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
 
-const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-sidenav/main/demo/apiExamples.md';
+const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-sidenav/main/demo/api.md';
 
 class AuroSidenavApi extends MarkdownPageWrapper {
 
