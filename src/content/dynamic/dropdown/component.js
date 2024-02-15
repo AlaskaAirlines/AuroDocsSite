ba4407ca44a4c4725784680ee1e9fc916ec636ac
Jordan Jones <jmjones2463@gmail.com>
2024-02-14 16:15:39 -0800

    perf(dropdown): update dropdown demo links

--- src/content/dynamic/dropdown/component.js
+++ src/content/dynamic/dropdown/component.js
@@ -5,7 +5,7 @@ import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
 import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
 
-const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-dropdown/main/demo/demo.md';
+const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-dropdown/main/demo/index.md';
 
 class AuroDropdown extends MarkdownPageWrapper {
 
