298ffb761f91f91eea503e0df5fdf438eefb1546
Jordan Jones <jmjones2463@gmail.com>
2024-02-20 12:04:42 -0800

    perf(loader): update loader demo links

--- src/content/dynamic/loader/component.js
+++ src/content/dynamic/loader/component.js
@@ -5,7 +5,7 @@ import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
 import { MarkdownPageWrapper } from 'components/markdownPageWrapper';
 
-const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-loader/master/demo/demo.md';
+const markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/auro-loader/main/demo/index.md';
 
 class AuroLoader extends MarkdownPageWrapper {
 
