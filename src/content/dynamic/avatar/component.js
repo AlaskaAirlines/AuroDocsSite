8e6ddc7c7c6eb234bffd8deefdf7f83d57ce24f9
Jason Baker <jason@capsule42.com>
2024-02-22 14:32:51 -0800

    docs(avatar): change MD paths for avatar component

--- src/content/dynamic/avatar/component.js
+++ src/content/dynamic/avatar/component.js
@@ -5,7 +5,7 @@ class AuroContent extends AuroComponentContent {
   constructor(props) {
     super(props);
 
-    this.markdownContentPath = 'demo/demo.md';
+    this.markdownContentPath = 'demo/index.md';
     this.hasCustomElementRegistration = false;
   };
 }
