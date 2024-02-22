67c796eac1470ea73065241a8710f8d3cd4c889e
Jason Baker <jason@capsule42.com>
2024-02-22 14:31:55 -0800

    fix(examples): change how JS examples are called

--- src/content/dynamic/toast/api.js
+++ src/content/dynamic/toast/api.js
@@ -1,13 +1,21 @@
 import AuroComponentContent from "functions/renderComponentPage";
+import { initExamples } from "@aurodesignsystem/auro-toast/demo/api.min.js";
 
 class AuroContent extends AuroComponentContent {
 
   constructor(props) {
     super(props);
 
+    this.hasExamples = false;
     this.markdownContentPath = 'demo/api.md';
+    this.hasCustomElementRegistration = false;
   };
+
+  componentDidMount() {
+    super.componentDidMount();
+
+    initExamples();
+  }
 }
 
 export default AuroContent;
-
