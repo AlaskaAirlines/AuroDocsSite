702aa0312487e0ad391402fe271819a4fd8be23d
Dale Sande <dale.sande@gmail.com>
2024-07-22 16:31:30 -0700

    docs: update design token references
    This commit updates the listed files to address changes with the Auro
    Design tokens repo. This includes removing references to deprecated
    concepts and adding in new concepts to be listed.
    
    On branch dsande/fix/tokens
    Changes to be committed:
    modified:   src/content/dynamic/color/digitalPalette.js
    modified:   src/content/dynamic/color/nav.js
    modified:   src/content/dynamic/designTokens/tokens.js

--- src/content/dynamic/color/nav.js
+++ src/content/dynamic/color/nav.js
@@ -8,7 +8,6 @@ export class Nav extends Component {
       <div role="tablist" className="tabList">
         <NavLink role="tab" exact className="tab link" to={`/color/overview`} activeClassName="is-active">Overview</NavLink>
         <NavLink role="tab" exact className="tab link" to={`/color/digital-palette`} activeClassName="is-active">Digital Palette</NavLink>
-        <NavLink role="tab" exact className="tab link" to={`/color/personality-palette`} activeClassName="is-active">Personality Palette</NavLink>
       </div>
     )
   }
