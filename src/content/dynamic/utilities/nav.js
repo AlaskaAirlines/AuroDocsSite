f4dbd1d2c3d161b7a1c02d375b8f1586d1c04778
rmenner <ryan.menner@alaskaair.com>
2024-11-22 16:07:21 -0500

    feat: routing and zip download setup for theme switcher page #274

---
+++ src/content/dynamic/utilities/nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+export class Nav extends Component {
+
+  render() {
+    return (
+      <div role="tablist" className="tabList">
+        <NavLink role="tab" end className="tab link" to={`/utilities/theme-switcher`} >Theme Switcher</NavLink>
+      </div>
+    )
+  }
+}
