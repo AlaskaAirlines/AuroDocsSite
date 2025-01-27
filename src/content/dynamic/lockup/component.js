fa2d0f1f84702fd874f35eb4e319817839bbd44b
Jordan Jones <jmjones2463@gmail.com>
2025-01-27 11:58:56 -0800

    fix: load lockup examples #293

--- src/content/dynamic/lockup/component.js
+++ src/content/dynamic/lockup/component.js
@@ -1,4 +1,5 @@
 import AuroComponentContent from "~/functions/renderComponentPage";
+import '@aurodesignsystem/auro-lockup/demo/index.min.js';
 import content from '@aurodesignsystem/auro-lockup/demo/index.md';
 
 class AuroContent extends AuroComponentContent {
