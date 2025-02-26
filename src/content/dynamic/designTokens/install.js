da5700182691dccfff631aee293167f54238fdfa
Chris Friedberg <chris.friedberg@alaskaair.com>
2025-02-26 15:05:46 -0800

    feat: update design token documentation to v5

--- src/content/dynamic/designTokens/install.js
+++ src/content/dynamic/designTokens/install.js
@@ -2,15 +2,13 @@ import AuroComponentContent from "~/functions/renderComponentPage";
 import { Nav } from './nav';
 
 class AuroContent extends AuroComponentContent {
-
   constructor(props) {
     super(props);
-
-    this.markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/AuroDesignTokens/v4.3.1/README.md';
+    this.markdownContent = 'https://raw.githubusercontent.com/AlaskaAirlines/AuroDesignTokens/master/README.md';
     this.packageName = '@aurodesignsystem/design-tokens';
     this.hasCustomElementRegistration = false;
   };
-
+  
   renderNav() {
     return (
       <Nav />
