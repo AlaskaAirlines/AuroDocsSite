0a7c036c0941f06cdafcfd88c51412f6c649b2ba
rmenner <ryan.menner@alaskaair.com>
2025-10-24 14:13:58 -0400

    fix: correct case in import path for README.md

--- src/content/dynamic/backtotop/install.js
+++ src/content/dynamic/backtotop/install.js
@@ -1,5 +1,5 @@
 import AuroComponentContent from "~/functions/renderComponentPage";
-import content from '@aurodesignsystem/auro-backtotop/readme.md';
+import content from '@aurodesignsystem/auro-backtotop/README.md';
 
 class AuroContent extends AuroComponentContent {
 
