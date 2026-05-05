e5ea03022eb2a60658d3bed6b3fbb5ba2e17bc2f
Jason Baker <jason@capsule42.com>
2026-05-04 18:35:24 -0700

    feat: update to include remaining formkit docs

---
+++ src/content/dynamic/form/keyboard-behavior.js
@@ -0,0 +1,25 @@
+import AuroComponentContent from "~/functions/renderFormkitComponentPage";
+import content from '@aurodesignsystem/auro-formkit/auro-form/demo/keyboard-behavior.md';
+
+class AuroContent extends AuroComponentContent {
+
+  constructor(props) {
+    super(props);
+
+    this.hasAccessibility = true;
+    this.hasVoiceover = true;
+    this.hasGettingStarted = true;
+    this.hasCustomize = true;
+    this.hasDesign = false;
+    this.hasKeyboardBehavior = true;
+    this.hasInstall = false;
+    this.hasSubNav = true;
+    this.markdownContent = content; 
+  };
+
+  componentDidMount() {
+    super.componentDidMount();
+  }
+}
+
+export default AuroContent;
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import fs from 'fs';
 import svgr from "vite-plugin-svgr";
+
+// Surge uses 200.html as the fallback for client-side routing
+function surge200Plugin() {
+    return {
+        name: 'surge-200-html',
+        closeBundle() {
+            const outDir = path.resolve(__dirname, 'dist');
+            const src = path.join(outDir, 'index.html');
+            const dest = path.join(outDir, '200.html');
+            if (fs.existsSync(src)) {
+                fs.copyFileSync(src, dest);
+            }
+        }
+    };
+}
 
 export default defineConfig({
     // depending on your application, base can also be "/"
     base: '/',
     publicDir: 'public',
-    plugins: [svgr(), react()],
+    plugins: [svgr(), react(), surge200Plugin()],
     assetsInclude: ['**/*.md', '**/*.zip'],
     resolve: {
         alias: {
